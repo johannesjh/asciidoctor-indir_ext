17a07880db4b0eca88b56d98486260a0d9b46610
johannesjh <johannesjh@users.noreply.github.com>
2018-11-03 13:30:05 +0100

    Initialized gem.

---
+++ lib/asciidoctor/indir_ext.rb
@@ -0,0 +1,8 @@
+require "asciidoctor/indir_ext/version"
+
+module Asciidoctor
+  module IndirExt
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/asciidoctor/indir_ext_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Asciidoctor::IndirExt do
+  it "has a version number" do
+    expect(Asciidoctor::IndirExt::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
